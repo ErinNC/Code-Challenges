28a033f60f18e85670442801101147e2d65e5cc8
Erin Costolo <96785813+ErinNC@users.noreply.github.com>
2022-09-27 09:38:56 -0400

    Query for cities not starting with vowels

--- HackerRank/SQL/WeatherObservationStation9.py
+++ HackerRank/SQL/WeatherObservationStation9.py
@@ -1,7 +1,7 @@
 # Query the list of CITY names from STATION that do not start with vowels.
 # Your result cannot contain duplicates.
 
-SELECT DISTINCT CITY
-FROM STATION
-WHERE CITY NOT
+SELECT DISTINCT city
+FROM station
+WHERE city NOT
       REGEXP '^[aeiou]'
