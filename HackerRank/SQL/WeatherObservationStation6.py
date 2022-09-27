9a3028cf9409fb3069b26c87ab95343b53843ed2
Erin Costolo <96785813+ErinNC@users.noreply.github.com>
2022-09-27 09:39:38 -0400

    Query for cities starting with vowel

--- HackerRank/SQL/WeatherObservationStation6.py
+++ HackerRank/SQL/WeatherObservationStation6.py
@@ -1,7 +1,7 @@
 # Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) 
 # from STATION. Your result cannot contain duplicates.
 
-SELECT DISTINCT CITY
-FROM STATION
-WHERE CITY
+SELECT DISTINCT city
+FROM station
+WHERE city
       REGEXP '^[aeiou]'
