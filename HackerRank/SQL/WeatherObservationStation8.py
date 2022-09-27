490db890193900e0ff620d270f935e079c1895a7
Erin Costolo <96785813+ErinNC@users.noreply.github.com>
2022-09-27 09:36:06 -0400

    Update WeatherObservationStation8.py

--- HackerRank/SQL/WeatherObservationStation8.py
+++ HackerRank/SQL/WeatherObservationStation8.py
@@ -2,7 +2,7 @@
 # (i.e., a, e, i, o, and u) as both their first and last 
 # characters. Your result cannot contain duplicates.
 
-SELECT DISTINCT CITY
-FROM STATION
-WHERE CITY
+SELECT DISTINCT city
+FROM station
+WHERE city
       REGEXP '^[aeiou].*[aeiou]$'
